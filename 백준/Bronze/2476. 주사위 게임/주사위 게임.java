aea1029c806221162c72e2ed6b3d7e0efa7d0ef0
haneol22 <93521167+haneol22@users.noreply.github.com>
2022-08-30 22:43:34 +0900

    [Bronze III] Title: 주사위 게임, Time: 320 ms, Memory: 23352 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.java
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.java
@@ -1,28 +1,31 @@
-import java.util.*;
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int[] sum = new int[num];
-		for(int i=0;i<num;i++)
-		{
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			int z = sc.nextInt();
-			
-			if(x == y && x == z && y == z)
-				sum[i] = 10000 + x*1000;
-			else if(x == y || x == z)
-				sum[i] = 1000 + x*100;
-			else if(y == z)
-				sum[i] = 1000 + y*100;
-			else {
-				int max = Math.max(x, Math.max(y, z));
-				sum[i] = max*100;
-			}
-		}
-		sc.close();
-		Arrays.sort(sum);
-		System.out.print(sum[sum.length - 1]+ " ");
-	}
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int N[] = new int[input];
+
+        for (int i = 0; i < input; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == b && a == c && b == c)
+                N[i] = 10000 + a * 1000;
+            else if (a == b || a == c)
+                N[i] = 1000 + a * 100;
+            else if (b == c)
+                N[i] = 1000 + b * 100;
+            else {
+                int max = Math.max(a, Math.max(b, c));
+                N[i] = max * 100;
+            }
+        }
+        sc.close();
+        Arrays.sort(N);
+        System.out.print(N[N.length - 1] + " ");
+    }
 }
+
